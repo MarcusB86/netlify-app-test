f4f53df866c59048e8bd77c64e4a0ce027a2ad93
MarcusB86 <marcusb08@gmail.com>
2024-03-30 15:22:17 -0400

    add text to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <h1><i>Dont Know What Im Doing</i></h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
