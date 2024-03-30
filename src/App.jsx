d7c2f07ea320d6bc76890b6d83cf912a8a5606a0
MarcusB86 <marcusb08@gmail.com>
2024-03-30 15:45:13 -0400

    updateappjsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <div> <h1><i>I Dont Know What Im Doing</i></h1></div>
     <h1><i>I Dont Know What Im Doing</i></h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
