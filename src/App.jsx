fb92a0f4c684906596b19744ffd91c1af7efa669
MarcusB86 <marcusb08@gmail.com>
2024-03-30 15:56:17 -0400

    removemessage

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
   return (
     <>
     <div> <h1><i>I Dont Know What Im Doing</i></h1></div>
-    <h1><i>I Dont Know What Im Doing</i></h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
